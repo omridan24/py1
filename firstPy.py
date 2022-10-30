3097d02fc140b6680a9ca0e7417a9f1ef7a86b50
Omri Dan <omridan24@gmail.com>
2022-10-30 15:38:39 +0200

    project is done

--- firstPy.py
+++ firstPy.py
@@ -7,12 +7,15 @@ def age():
     your_age = year - birth_year
     print( "your age is " + str(your_age))
 def best_stocks():
-    print ("the best stocks can be found on : investors.com")
+    return ("the best stocks can be found on : investors.com")
 
+
+def best_books():
+    return("U can find best books of the year at : https://time.com/6178674/best-books-2022-so-far/")
 name = input("what is your name? ")
 print ("hello " + name)
 answer = input("do you want to know your age? ")
 if(answer == "yes"):
     age()
-else:
-    best_stocks()
+elif(answer == "no"):
+    print ("than look at that:" + best_stocks() + best_books())
